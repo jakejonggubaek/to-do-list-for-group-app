f231dffa22f70fd3a705fc2d251d6ae597002e55
jakejonggubaek <jakejonggubaek@gmail.com>
2021-04-11 14:30:29 -0400

    add public_url

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 function App() {
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
         <main>
