50514a0f6ba19dc656ff35dca1bf5e6660d437e6
jakejonggubaek <jakejonggubaek@gmail.com>
2021-05-16 22:45:53 -0400

    appled responsive UI

--- src/Room.js
+++ src/Room.js
@@ -20,6 +20,10 @@ function Room(props) {
     const [displayedToDoList, setDisplayedToDoList] = useState([]);
     const [allDone, setAllDone] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
+    const [screenWidth, setScreenWidth] = useState(0);
+    const [isChat, setIsChat] = useState(false);
+    const [isList, setIsList] = useState(true);
+    const [isNote, setIsNote] = useState(false);
 
     const roomId = props.match.params.uniqueKey;
 
@@ -38,6 +42,12 @@ function Room(props) {
         });
 
     }, [roomId])
+
+    useEffect(() => {
+        setScreenWidth(window.innerWidth);
+        window.addEventListener("resize", handleResize);
+
+    }, [screenWidth])
 
     useEffect(() => {
         axios.put("https://team-to-do-app.herokuapp.com/update",
@@ -64,7 +74,56 @@ function Room(props) {
             setAllDone(false);
         }
 
-    }, [toDoList, roomId])
+    }, [toDoList, roomId]);
+
+    const handleResize = (e) => {
+        setScreenWidth(window.innerWidth);
+    };
+
+    const chatToggle = () => {
+        
+        const chat = document.querySelector('.chat');
+        const list = document.querySelector('.to-do-list-container');
+        const note = document.querySelector('.note');
+
+        chat.style.display = 'block';
+        list.style.display = 'none';
+        note.style.display = 'none';
+        
+        setIsChat(true);
+        setIsList(false);
+        setIsNote(false);
+    }
+
+    const toDoToggle = () => {
+
+        const chat = document.querySelector('.chat');
+        const list = document.querySelector('.to-do-list-container');
+        const note = document.querySelector('.note');
+
+        chat.style.display = 'none';
+        list.style.display = 'block';
+        note.style.display = 'none';
+
+        setIsChat(false);
+        setIsList(true);
+        setIsNote(false);
+    }
+
+    const noteToggle = () => {
+
+        const chat = document.querySelector('.chat');
+        const list = document.querySelector('.to-do-list-container');
+        const note = document.querySelector('.note');
+
+        chat.style.display = 'none';
+        list.style.display = 'none';
+        note.style.display = 'block';
+
+        setIsChat(false);
+        setIsList(false);
+        setIsNote(true);
+    }
 
     return(
         <>
@@ -73,11 +132,22 @@ function Room(props) {
                 {authorized?
                 <div className="room">
                     <h2>Hi! {userName}, welcome to the room for {roomName}!</h2>
-                    <div className="main-container">
-                                <Chat roomId={roomId} userName={userName} roomName={roomName}/>
-                        <ToDoList displayedToDoList={displayedToDoList} allDone={allDone} toDo={toDo} setToDoList={setToDoList} setToDo={setToDo} toDoList={toDoList} />
-                        <Note />
+
+                            <div className="main-container">
+                                <Chat roomId={roomId} userName={userName} roomName={roomName} />
+                                <ToDoList displayedToDoList={displayedToDoList} allDone={allDone} toDo={toDo} setToDoList={setToDoList} setToDo={setToDo} toDoList={toDoList} />
+                                <Note />
+                            </div>
+                    { screenWidth > 900
+                    ?
+                    <></>
+                    :
+                    <div className="button-container">
+                        {isChat ? <></> : <button className="chat-toggle" onClick={chatToggle}>Chat</button> }
+                        {isList ? <></> : <button className="toDo-toggle" onClick={toDoToggle}>List</button> }
+                        {isNote ? <></> : <button className="note-toggle" onClick={noteToggle}>Note</button> }
                     </div>
+                    }
                     
                 </div>
                         : <Password userName={userName} password={password} isPassword={isPassword} setUserName={setUserName} setAuthorized={setAuthorized}/>
